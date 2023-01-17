5b3c5971796d1025eb1c184f5b264a92d55f2131
datdadev <112007525+datdadev@users.noreply.github.com>
2023-01-17 13:51:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,8 @@ Author: DatDaDev
 Version: 0.0.1
 Description:
 - Displays the today's and tomorrow's timetable of subjects for students studying at Ho Chi Minh University of Techology (HCMUT).
-- Works with medium-sized iOS widget only.
-- Restricted displaying 3 subjects for each day.
-Usage:
-1. Visit https://mybk.hcmut.edu.vn/stinfo/ then login into your account.
-2. Click at timetable tab.
-3. Copy all your timetable in the selected semeter as text, except for the row header.
-4. Paste it into the parameter text field of the medium-sided widget.
+- Better works with medium-sized or larger size iOS widget for the better view.
+Usage: Visit https://github.com/datdadev/BK-Widget
 -------------------------------------------------------------- */
 
 const widget = new ListWidget();
